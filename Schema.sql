a0facd760eb30fcd9e4985e053aa3f5b3ac538f3
Christopher Watkins <chrisbwatkins@gmail.com>
2021-02-25 20:38:06 -0700

    getting the crud for me roles

--- Schema.sql
+++ Schema.sql
@@ -6,17 +6,17 @@ CREATE TABLE department(
     names VARCHAR(30)
 );
 CREATE TABLE roles(
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL(10,2),
-    department_id INT
+   FOREIGN KEY (department_id) REFERENCES department(id)
 );
 CREATE TABLE employee(
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT NULL
+  FOREIGN KEY role_id REFERENCES roles(id),
+  FOREIGN key manager_id REFERENCES employee(id) INT NULL
 );
 SELECT * FROM department;
 SELECT * FROM roles;
--- server.js
+++ server.js
@@ -28,7 +28,7 @@ function Employment(){
             
             break;
             case'roles':
-            //function for roles
+           roleChoices();
             break;
             case'employee':
             //function for employee
@@ -42,6 +42,8 @@ function Employment(){
 
     })
 }
+
+
 
 function departmentChoices(){
     inquirer.prompt([
@@ -113,17 +115,118 @@ const deleteDepartment = () =>{
             delet('department',response.departmentDeletion)
             Employment();
         })
-    }  )
+    })
+}
+
+function roleChoices(){
+    inquirer.prompt([
+        {
+            type:'list',
+            message:"would you like do",
+            choices: ['view the roles','add a role',"delete a role","go back"],
+            name:"roleChoice"
+        }
+    ]).then((response)=>{
+        console.log(response.roleChoice);
+        switch(response.roleChoice){
+            case'view the roles':
+            read("roles");
+            break;
+            case'add a role':
+            addRole();
+            break;
+            case'delete a role':
+            deleteRole();
+            break;
+            case'go back':
+            Employment();
+            break;
+
+        }
+
+    })
 }
 
 
 
-const create = (where,what) =>{
+function addRole(){
+    connection.query('SELECT * FROM department',(err,data)=>{
+        if(err) throw err;
+        depid = data.map(({name,id})=>({
+            name:name,
+            value:id
+        }))
+    inquirer.prompt([
+        {
+            message:'what role would you like to add?',
+            name:'title'
+        },
+        {
+            message:'how much is the salary?',
+            name:'salary'
+        },
+        {
+            type:'rawlist',
+            message:'which department is the role assigned to?',
+            choices:depid,
+            name:'departmentID'
 
-    const query = connection.query(`INSERT INTO ${where} SET ?`,{
-        names:what,
 
-    },(err,data)=>{
+        }
+  
+        ]).then((response)=>{
+            console.log(response.title);
+            create('roles',response.title,response.salary,parseInt(response.departmentID))
+
+        })
+    })
+}
+
+function deleteRole(){
+    connection.query(`SELECT * FROM roles`,(err,data)=>{
+        if(err) throw err;
+        rolearray = []
+        data.forEach(id => {
+            rolearray.push(id.title);
+        })
+        console.log(rolearray);
+    inquirer.prompt([
+        {
+            type:'rawlist',
+            message:'which role do you wanna delete?',
+            choices:rolearray,
+            name:'roledeletion'
+        }
+    ]).then((response)=>{
+        console.log(response);
+        delet('roles',response.roledeletion);
+        Employment();
+    })
+})
+
+}
+
+
+const create = (where,...what) =>{
+   
+    var whats = []
+    switch(where){
+        case 'department':
+        whats = {names:what};
+        break;
+        case 'roles':
+        whats = {title:what[0],
+                salary:what[1],
+                department_id:what[2],
+
+        };
+        break;
+    }
+
+
+
+    const query = connection.query(`INSERT INTO ${where} SET ?`,
+    whats,(err,data)=>{
         if (err) throw err;
         console.log("Create");
         Employment();
@@ -163,11 +266,27 @@ const updata = () => {
 }
 
 const delet = (where,data) =>{
-    connection.query(
-        `DELETE FROM ${where}  WHERE ?`,
+    if(where){
+        if(where == 'department')
         {
-           names:`${data}`
-        },
+            deletewhat =  
+            {
+            names: `${data}`
+         }
+        }
+        else if(where == 'roles')
+        {
+            deletewhat = {
+                title:`${data}`
+            }
+
+
+
+        }
+    }
+    connection.query(
+        `DELETE FROM ${where}  WHERE ?`,deletewhat
+,
         (err,data) =>{
             if(err) throw err,
             console.log('delete')
